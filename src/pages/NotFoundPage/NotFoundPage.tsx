75002227ff113b203de04d3731920a2453388272
Svetlana-Ts <svetlana.tsibenova@gmail.com>
2025-06-18 11:22:26 +0800

    4.21 refactor Not Found Page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,36 +1,16 @@
-import HeaderLeft from '../../components/header/ui/HeaderLeft/HeaderLeft';
 import { Link } from 'react-router-dom';
 import './NotFoundPage.css';
+import { AppRoute } from '../../const';
 
 function NotFoundPage() {
   return (
-    <div className="page page--gray page--login">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <HeaderLeft />
-          </div>
-        </div>
-      </header>
-
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="title">Page not found</h1>
-            <Link className="link-to-main" to="/">
-              Go to main page
-            </Link>
-          </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="#">
-                <span>Amsterdam</span>
-              </a>
-            </div>
-          </section>
-        </div>
-      </main>
-    </div>
+      <div className='not-found-page--container'>
+          <div className='not-found-page--404'>404</div>
+          <div className='not-found-page--title'>Page not found</div>
+          <Link className='link-to-main' to={AppRoute.Main}>
+            Go to main page
+          </Link>
+      </div>
   );
 }
 
