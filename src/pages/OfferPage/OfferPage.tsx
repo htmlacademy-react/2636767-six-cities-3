a9cab2e9de07c11ff2c317af0975aa45b2ece330
Svetlana-Ts <svetlana.tsibenova@gmail.com>
2025-06-20 11:59:17 +0800

    4.21 added throw exception to offer page

--- src/pages/OfferPage/OfferPage.tsx
+++ src/pages/OfferPage/OfferPage.tsx
@@ -1,57 +1,44 @@
+import { useParams } from 'react-router-dom';
 import RewiewForm from '../../components/rewiewForm/RewiewForm';
 import { AuthorizationStatus } from '../../const';
+import { mockOffersList } from '../../mockData/offers';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 interface OfferPageProps {
   authStatus: AuthorizationStatus;
 }
 
 function OfferPage({ authStatus }: OfferPageProps) {
+  const { id } = useParams();
+
+  const currentOffer = mockOffersList.find((mockOffer) => mockOffer.id === id);
+
+  if (!currentOffer) {
+    return <NotFoundPage />;
+  }
+
   return (
     <main className='page__main page__main--offer'>
       <section className='offer'>
         <div className='offer__gallery-container container'>
           <div className='offer__gallery'>
             <div className='offer__image-wrapper'>
-              <img
-                className='offer__image'
-                src='img/room.jpg'
-                alt='Photo studio'
-              />
+              <img className='offer__image' src='img/room.jpg' alt='Photo studio' />
             </div>
             <div className='offer__image-wrapper'>
-              <img
-                className='offer__image'
-                src='img/apartment-01.jpg'
-                alt='Photo studio'
-              />
+              <img className='offer__image' src='img/apartment-01.jpg' alt='Photo studio' />
             </div>
             <div className='offer__image-wrapper'>
-              <img
-                className='offer__image'
-                src='img/apartment-02.jpg'
-                alt='Photo studio'
-              />
+              <img className='offer__image' src='img/apartment-02.jpg' alt='Photo studio' />
             </div>
             <div className='offer__image-wrapper'>
-              <img
-                className='offer__image'
-                src='img/apartment-03.jpg'
-                alt='Photo studio'
-              />
+              <img className='offer__image' src='img/apartment-03.jpg' alt='Photo studio' />
             </div>
             <div className='offer__image-wrapper'>
-              <img
-                className='offer__image'
-                src='img/studio-01.jpg'
-                alt='Photo studio'
-              />
+              <img className='offer__image' src='img/studio-01.jpg' alt='Photo studio' />
             </div>
             <div className='offer__image-wrapper'>
-              <img
-                className='offer__image'
-                src='img/apartment-01.jpg'
-                alt='Photo studio'
-              />
+              <img className='offer__image' src='img/apartment-01.jpg' alt='Photo studio' />
             </div>
           </div>
         </div>
@@ -61,9 +48,7 @@ function OfferPage({ authStatus }: OfferPageProps) {
               <span>Premium</span>
             </div>
             <div className='offer__name-wrapper'>
-              <h1 className='offer__name'>
-                Beautiful &amp; luxurious studio at great location
-              </h1>
+              <h1 className='offer__name'>Beautiful &amp; luxurious studio at great location</h1>
               <button className='offer__bookmark-button button' type='button'>
                 <svg className='offer__bookmark-icon' width='31' height='33'>
                   <use xlinkHref='#icon-bookmark'></use>
@@ -79,15 +64,9 @@ function OfferPage({ authStatus }: OfferPageProps) {
               <span className='offer__rating-value rating__value'>4.8</span>
             </div>
             <ul className='offer__features'>
-              <li className='offer__feature offer__feature--entire'>
-                Apartment
-              </li>
-              <li className='offer__feature offer__feature--bedrooms'>
-                3 Bedrooms
-              </li>
-              <li className='offer__feature offer__feature--adults'>
-                Max 4 adults
-              </li>
+              <li className='offer__feature offer__feature--entire'>Apartment</li>
+              <li className='offer__feature offer__feature--bedrooms'>3 Bedrooms</li>
+              <li className='offer__feature offer__feature--adults'>Max 4 adults</li>
             </ul>
             <div className='offer__price'>
               <b className='offer__price-value'>&euro;120</b>
@@ -125,14 +104,13 @@ function OfferPage({ authStatus }: OfferPageProps) {
               </div>
               <div className='offer__description'>
                 <p className='offer__text'>
-                  A quiet cozy and picturesque that hides behind a a river by
-                  the unique lightness of Amsterdam. The building is green and
-                  from 18th century.
+                  A quiet cozy and picturesque that hides behind a a river by the unique lightness
+                  of Amsterdam. The building is green and from 18th century.
                 </p>
                 <p className='offer__text'>
-                  An independent House, strategically located between Rembrand
-                  Square and National Opera, but where the bustle of the city
-                  comes to rest in this alley flowery and colorful.
+                  An independent House, strategically located between Rembrand Square and National
+                  Opera, but where the bustle of the city comes to rest in this alley flowery and
+                  colorful.
                 </p>
               </div>
             </div>
@@ -162,9 +140,8 @@ function OfferPage({ authStatus }: OfferPageProps) {
                       </div>
                     </div>
                     <p className='reviews__text'>
-                      A quiet cozy and picturesque that hides behind a a river
-                      by the unique lightness of Amsterdam. The building is
-                      green and from 18th century.
+                      A quiet cozy and picturesque that hides behind a a river by the unique
+                      lightness of Amsterdam. The building is green and from 18th century.
                     </p>
                     <time className='reviews__time' dateTime='2019-04-24'>
                       April 2019
@@ -180,9 +157,7 @@ function OfferPage({ authStatus }: OfferPageProps) {
       </section>
       <div className='container'>
         <section className='near-places places'>
-          <h2 className='near-places__title'>
-            Other places in the neighbourhood
-          </h2>
+          <h2 className='near-places__title'>Other places in the neighbourhood</h2>
           <div className='near-places__list places__list'>
             <article className='near-places__card place-card'>
               <div className='near-places__image-wrapper place-card__image-wrapper'>
@@ -200,19 +175,13 @@ function OfferPage({ authStatus }: OfferPageProps) {
                 <div className='place-card__price-wrapper'>
                   <div className='place-card__price'>
                     <b className='place-card__price-value'>&euro;80</b>
-                    <span className='place-card__price-text'>
-                      &#47;&nbsp;night
-                    </span>
+                    <span className='place-card__price-text'>&#47;&nbsp;night</span>
                   </div>
                   <button
                     className='place-card__bookmark-button place-card__bookmark-button--active button'
                     type='button'
                   >
-                    <svg
-                      className='place-card__bookmark-icon'
-                      width='18'
-                      height='19'
-                    >
+                    <svg className='place-card__bookmark-icon' width='18' height='19'>
                       <use xlinkHref='#icon-bookmark'></use>
                     </svg>
                     <span className='visually-hidden'>In bookmarks</span>
@@ -247,19 +216,10 @@ function OfferPage({ authStatus }: OfferPageProps) {
                 <div className='place-card__price-wrapper'>
                   <div className='place-card__price'>
                     <b className='place-card__price-value'>&euro;132</b>
-                    <span className='place-card__price-text'>
-                      &#47;&nbsp;night
-                    </span>
+                    <span className='place-card__price-text'>&#47;&nbsp;night</span>
                   </div>
-                  <button
-                    className='place-card__bookmark-button button'
-                    type='button'
-                  >
-                    <svg
-                      className='place-card__bookmark-icon'
-                      width='18'
-                      height='19'
-                    >
+                  <button className='place-card__bookmark-button button' type='button'>
+                    <svg className='place-card__bookmark-icon' width='18' height='19'>
                       <use xlinkHref='#icon-bookmark'></use>
                     </svg>
                     <span className='visually-hidden'>To bookmarks</span>
@@ -297,19 +257,10 @@ function OfferPage({ authStatus }: OfferPageProps) {
                 <div className='place-card__price-wrapper'>
                   <div className='place-card__price'>
                     <b className='place-card__price-value'>&euro;180</b>
-                    <span className='place-card__price-text'>
-                      &#47;&nbsp;night
-                    </span>
+                    <span className='place-card__price-text'>&#47;&nbsp;night</span>
                   </div>
-                  <button
-                    className='place-card__bookmark-button button'
-                    type='button'
-                  >
-                    <svg
-                      className='place-card__bookmark-icon'
-                      width='18'
-                      height='19'
-                    >
+                  <button className='place-card__bookmark-button button' type='button'>
+                    <svg className='place-card__bookmark-icon' width='18' height='19'>
                       <use xlinkHref='#icon-bookmark'></use>
                     </svg>
                     <span className='visually-hidden'>To bookmarks</span>
